10f55f3bd75e22ff7fdcd542c9b39b197d134d4c
ShafaetUllah032 <53017129+ShafaetUllah032@users.noreply.github.com>
2023-12-24 19:09:13 +0600

    sorting algo added

---
+++ learnProgramming/sortingAlgo.cpp
@@ -0,0 +1,190 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ 1. selection sort focus one selecting the minimum one from the array(i>0) and swap with the ith number.
+     
+void selection_sort(int arr[], int n) {
+  // selection sort
+  for (int i = 0; i < n - 1; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+    int temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
+
+
+void bubble_sort(int arr[], int n) {
+    // bubble sort
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "After Using bubble sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+
+
+
+
+**/
+
+
+void merge(vector<int>&arr,int low,int mid,int high)
+{
+    vector<int>temp;
+    int left=low;
+    int right=mid+1;
+
+    while(left<=mid && right <=high)
+    {
+        if(arr[left]<arr[right])
+        {
+            temp.push_back(arr[left]);
+            left ++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right ++;
+        }
+    }
+    while(left<=mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high)
+    {
+        temp.push_back(arr[right]);
+        right ++;
+    }
+
+    for(int i=low;i<=high;i++)
+    {
+        arr[i]=temp[i-low];
+    }
+
+}
+
+void mergeSort(vector<int>&arr,int low,int high)
+{
+    if(low>=high)
+    {
+        return ;
+    }
+
+    int mid =(low+high)/2;
+    mergeSort(arr,low,mid);
+    mergeSort(arr,mid+1,high);
+    merge(arr,low,mid,high);
+
+}
+
+
+int main() {
+
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+    int n = 7;
+
+    cout << "Before Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    return 0 ;
+}
+
+
+/**
+ int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<int> arr) {
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 6, 2, 5, 7, 9, 1, 3};
+    int n = arr.size();
+    cout << "Before Using quick Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    arr = quickSort(arr);
+    cout << "After Using quick sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
+**/
