298c9bba48497875baa774cb1c20ae80f6691054
ShafaetUllah032 <53017129+ShafaetUllah032@users.noreply.github.com>
2024-03-23 11:06:49 +0600

    parameter passing

---
+++ learnProgramming/function.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+
+void doSomething(int num)
+{
+    cout<<num<<endl;
+    num +=5;
+    cout<<num<<endl;
+
+    num +=5;
+    cout<<num<<endl;
+    
+}
+
+void doExecptional( int &num) /// pass by referance 
+{
+    cout<<num<<endl;
+    num +=10;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n=0;
+    cout<<n<<endl;
+    doSomething(n); /// pass by value
+    cout<<n<<endl;
+
+    n=11;
+    cout<<n<<endl;
+
+    doExecptional(n);
+
+    cout<<n<<endl;
+
+    return 0;
+}
+/** মৃতরা কি ব্যাথা পায় ? যখন দেহ মাটিতে খায় ! **/
+/**LaZyBadB0y **/
