78b111cd30c790f6bd7af574a69154eb68a118d0
ShafaetUllah032 <53017129+ShafaetUllah032@users.noreply.github.com>
2024-03-25 07:13:50 +0600

    basic recursion added

--- learnProgramming/recurrsion.cpp
+++ learnProgramming/recurrsion.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define gc getchar_unlocked
-#define fo(i, n) for (i = 0; i < n; i++)
+#define fo(i, n) for (int i = 0; i < n; i++)
 #define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
 #define ll long long
 #define deb(x) cout << #x << "=" << x << endl
@@ -23,6 +23,88 @@ typedef vector<pii> vpii;
 typedef vector<pl> vpl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
+
+void recur(int n) // printing number continuously without loop 
+{
+
+    if(n==404)
+    {
+        cout<<"not found "<<endl;
+        return ;
+    }
+    cout<<n<<endl;
+    recur(n+1);
+
+    
+}
+
+vector<string> recurString(int n) /// pushing on string vector without loop 
+{
+
+    vector<string> data;
+    cout<<"called for " <<n<<endl<<data.size()<<endl;
+
+    if(n>0)
+    {
+        data=recurString(n-1); // updated as return 
+        cout<<"responded for "<<n<<" "<<data.size()<<endl;
+        //cout<<data.size()<<" "<<n<<endl;
+        data.push_back("this is Safaet");
+    }
+    return data;
+}
+
+
+int sumRecur(int n)
+{
+    ll val=0;
+    if(n>0)
+    {
+        val = sumRecur(n-1);
+        
+        val +=n;
+        cout<<val<<endl;
+
+    }
+    return val;
+}
+
+
+int factoRecur(int n)
+{
+    ll val=1;
+    /*
+    
+    if(n==0)
+    {
+        return 1;
+    }
+    factoRecur(n)=n*factoRecur(n-1);
+
+    */
+
+    if(n>0)
+    {
+        val = factoRecur(n-1)*n;
+        //cout<<n<<" "<<val<<endl;
+
+    }
+    return val;
+}
+
+void revArray(vector<int> &data,int start,int end)
+{
+    if(start<end)
+    {
+        swap(data[start],data[end]);
+        revArray(data,start+1,end-1);
+        cout<<"swap done "<<endl;
+    }
+    return ;
+}
+
+
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -32,12 +114,29 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int t,n,x,y,a,b;
-    cin >> t;
-    while (t--)
+    int n;
+    cin>>n;
+
+    //recur(n);
+    vector<string> dstr;
+    //dstr=recurString(n);
+    //cout<<dstr.size()<<endl;
+
+    //ll val=sumRecur(5);
+    //cout<<val<<endl;
+    ll fac=factoRecur(n);
+    cout<<fac<<endl;
+
+    vi data={1,2,3,4,5};
+
+    revArray(data,0,data.size()-1);
+    for(int i=0;i<data.size();i++)
     {
-        //write code here
+        cout<<data[i]<<endl;
     }
+
+
+
     return 0;
 }
 /** মৃতরা কি ব্যাথা পায় ? যখন দেহ মাটিতে খায় ! **/
